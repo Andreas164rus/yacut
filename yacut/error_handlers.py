26f6ba954e088f212a433147fb6a6d289df3da3a
Andrei <Andreas164@yandex.ru>
2025-01-27 18:10:39 +0400

    three review

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from flask import render_template, jsonify
 
 from . import app
@@ -6,7 +8,7 @@ from .exceptions import InvalidAPIUsage
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
 @app.errorhandler(InvalidAPIUsage)
--- yacut/validation.py
+++ yacut/validation.py
@@ -7,7 +7,5 @@ def validation_short_link(short_link):
     if len(short_link) > MAX_LENGHT_SHORT_LINK:
         return False
     validated_part_short_link = re.search(REGEX_VALIDATE, short_link)
-    if (validated_part_short_link and
-            validated_part_short_link.group() == short_link):
-        return True
-    return False
+    return (validated_part_short_link and
+            validated_part_short_link.group() == short_link)
